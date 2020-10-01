93fe778139bf8fcac63c29ed2bf9948adf8a5669
nickupx <nick.upsky@gmail.com>
2020-10-01 20:40:29 +0300

    test fix

--- src/js/__tests__/map.test.js
+++ src/js/__tests__/map.test.js
@@ -5,7 +5,7 @@ const myErrorRepo = new ErrorRepository()
 myErrorRepo.add(404, 'Page not found')
 myErrorRepo.add(500, 'Server error')
 
-test('should return code', () => {
+test('should return description', () => {
   expect(myErrorRepo.translate(404)).toBe('Page not found')
 })
 
